499964e29cff3569ce6caaf51a0c55bd6429b316
hongnhung2684 <nhungnthph45436@fpt.edu.vn>
2024-11-05 09:34:08 +0700

    init

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminProductPage = () => {
+  return <div></div>;
+};
+
+export default AdminProductPage;
---
+++ app/website/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageAbout = () => {
+  return <div>PageAbout</div>;
+};
+
+export default PageAbout;
